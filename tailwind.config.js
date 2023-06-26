8cd29c19e7ff66b8153f0b375f2e53884ab5b912
Hugo Tiburcio <hugotiburcio@outlook.com>
2023-06-25 22:23:24 -0300

    Initial Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      colors: {
+        "very-dark-blue": "#1b1d23",
+        "very-light-gray": "#eeeff4",
+        "light-gray": "#c8ccd8",
+        "medium-gray": "#7d828f",
+        "dark-gray": "#60636d",
+        red: "#df5656",
+      },
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [],
-}
+};
